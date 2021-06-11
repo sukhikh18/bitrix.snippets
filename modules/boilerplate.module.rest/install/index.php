a1c71c5ef41094f72e60175b8e779d8128780169
NikolaySuhih <nikolay.suhih@nutnet.ru>
2021-06-11 10:58:51 +0400

    Make new feature :construction:

---
+++ modules/boilerplate.module.rest/install/index.php
@@ -0,0 +1,84 @@
+<?php
+
+use Bitrix\Main;
+
+if (class_exists('boilerplate_module')) {
+    return;
+}
+
+class boilerplate_module extends CModule
+{
+
+    public function __construct()
+    {
+        IncludeModuleLangFile(__FILE__);
+
+        $arModuleVersion = ['VERSION' => '', 'VERSION_DATE' => ''];
+        include __DIR__ . '/version.php';
+        $this->MODULE_ID = GetModuleID(__DIR__);
+        $this->MODULE_VERSION = $arModuleVersion['VERSION'];
+        $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
+        $this->MODULE_NAME = 'Пример модуля';
+        $this->MODULE_DESCRIPTION = 'Тестовый модуль для разработчиков, можно использовать как основу для разработки новых модулей для 1С:Битрикс';
+        $this->PARTNER_NAME = '';
+        $this->PARTNER_URI = '';
+    }
+
+    function InstallDB()
+    {
+    }
+
+    function InstallEvents()
+    {
+    }
+
+    function InstallFiles()
+    {
+    }
+
+    function DoInstall()
+    {
+        if (!CheckVersion(Main\ModuleManager::getVersion('main'), '14.00.00')) {
+            Main\Context::getApplication()->ThrowException('Версия главного модуля ниже 14. Не поддерживается технология D7, необходимая модулю. Пожалуйста обновите систему.');
+        }
+
+        $this->InstallDB();
+        $this->InstallEvents();
+        $this->InstallFiles();
+
+        Main\ModuleManager::RegisterModule($this->MODULE_ID);
+    }
+
+    function UnInstallDB()
+    {
+    }
+
+    function UnInstallEvents()
+    {
+    }
+
+    function UnInstallFiles()
+    {
+    }
+
+    function DoUninstall()
+    {
+        $this->UnInstallDB();
+        $this->UnInstallEvents();
+        $this->UnInstallFiles();
+
+        Main\ModuleManager::UnRegisterModule($this->MODULE_ID);
+    }
+
+    /**
+     * Get module holder folder name.
+     * @return string /document/local (when exists) or /document/bitrix
+     */
+    public static function getRoot()
+    {
+        $local = $_SERVER['DOCUMENT_ROOT'] . '/local';
+        if (false !== strpos(__DIR__, 'local' . DIRECTORY_SEPARATOR . 'modules') && is_dir($local)) {
+            return $local;
+        }
+    }
+}
