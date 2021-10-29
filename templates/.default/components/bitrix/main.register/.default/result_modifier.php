bc83b5ef3ea86395db8a95aa04918ba880cbf8e9
NikolaySuhih <nikolay.suhih@nutnet.ru>
2021-10-29 17:31:18 +0400

    Fix :bug:

--- templates/.default/components/bitrix/main.register/.default/result_modifier.php
+++ templates/.default/components/bitrix/main.register/.default/result_modifier.php
@@ -10,5 +10,5 @@
 $this->getComponent()->SetResultCacheKeys(['ERRORS']);
 
 // Sort fields by params.
-$arResult["SHOW_FIELDS"] = array_unique(array_merge($arParams['SHOW_FIELDS'], $arResult["SHOW_FIELDS"],
-    array_keys($arResult["USER_PROPERTIES"]["DATA"])));
+$arResult["SHOW_FIELDS"] = array_unique(array_merge($arParams['SHOW_FIELDS'], $arResult["SHOW_FIELDS"] ?: [],
+    array_keys($arResult["USER_PROPERTIES"]["DATA"] ?: [])));
